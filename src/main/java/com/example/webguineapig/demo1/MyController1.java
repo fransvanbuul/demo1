10b54554794a4cab3f767c21f949b9f6038b099c
Frans van Buul <frans.buul@microfocus.com>
2020-05-01 10:03:28 +0200

    stuff

--- src/main/java/com/example/webguineapig/demo1/MyController1.java
+++ src/main/java/com/example/webguineapig/demo1/MyController1.java
@@ -1,28 +1,43 @@
 package com.example.webguineapig.demo1;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 @Controller
 @RequestMapping("/ctrl1")
 public class MyController1 {
 
 	@GetMapping("/bla")
-	public void bla(@RequestParam String sql) {
-		Connection c;
+	public void bla(@RequestParam String xml) {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setExpandEntityReferences(false);
+		DocumentBuilder db;
 		try {
-			c = DriverManager.getConnection("bla");
-			c.createStatement().execute(sql);
-		} catch (SQLException e) {
+			db = dbf.newDocumentBuilder();
+			Document document = db.parse(xml);	
+		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
